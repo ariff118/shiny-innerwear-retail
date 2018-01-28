88a29fc98c6974bfbbc1e7a8f7ccbce71a38cfe3
Ha Dinh <dinhhn.ubc@gmail.com>
2018-01-28 11:37:20 -0800

    created reactive action to update selectInput when include all is selected

--- src/app/shiny-innerwear/app.R
+++ src/app/shiny-innerwear/app.R
@@ -30,9 +30,10 @@ ui <- fluidPage(
        checkboxInput("allbrandInput", "Include all brands", value = TRUE),
        
        uiOutput('categoryOutput'),
-       checkboxInput("allcategoryInput", "Include all categories", value = FALSE),
+       checkboxInput("allcategoryInput", "Include all categories", value = TRUE),
        
        uiOutput('colorOutput'),
+       # checkboxInput("allcolorInput", "Include all color options", value = FALSE),
        
        sliderInput("ratingInput", "Average Rating Range",
                    min = 1,
@@ -72,7 +73,7 @@ ui <- fluidPage(
 #===================#
 
 # Define server logic required to draw a histogram
-server <- function(input, output){
+server <- function(input, output, session){
   
   # set renderUI for interactive input selection
   output$brandOutput <- renderUI({
@@ -99,33 +100,6 @@ server <- function(input, output){
   
   # filter data for visualization
   filtered <- reactive({
-    # if(input$allbrandInput){
-    #   df %>% 
-    #     filter(category %in% c(input$categoryInput),
-    #            color_group %in% c(input$colorInput),
-    #            rating >= input$ratingInput[1],
-    #            rating <= input$ratingInput[2],
-    #            price >= input$priceInput[1],
-    #            price <= input$priceInput[2])
-    # }
-    # else if (input$allcategoryInput){
-    #   df %>% 
-    #     filter(brand %in% c(input$brandInput),
-    #            color_group %in% c(input$colorInput),
-    #            rating >= input$ratingInput[1],
-    #            rating <= input$ratingInput[2],
-    #            price >= input$priceInput[1],
-    #            price <= input$priceInput[2])
-    # }
-    # else if ( (input$allcategoryInput) & (input$allbrandInput)){
-    #   df %>% 
-    #     filter(color_group %in% c(input$colorInput),
-    #            rating >= input$ratingInput[1],
-    #            rating <= input$ratingInput[2],
-    #            price >= input$priceInput[1],
-    #            price <= input$priceInput[2])
-    # }
-    # else {
       df %>% 
         filter(brand %in% c(input$brandInput),
                category %in% c(input$categoryInput),
@@ -134,30 +108,40 @@ server <- function(input, output){
                rating <= input$ratingInput[2],
                price >= input$priceInput[1],
                price <= input$priceInput[2])
-    # }
   })
   
-  # when choose "Include all categories", update SelectInput to include all categories
-  # observe({
-  #   if (input$allcategoryInput){
-  #     updateSelectInput(session,
-  #                       inputId = "categoryInput",
-  #                       sort(unique(df$category)),
-  #                       selected = c(unique(df$category))
-  #                       )
-  #   }
-  # })
-  # 
   # when choose "Include all brands", update SelectInput to include all brands
-  # observe({
-  #   if (input$allbrandInput){
-  #     updateSelectInput(session,
-  #                       inputId = "brandInput",
-  #                       sort(unique(df$brand)),
-  #                       selected = c(unique(df$brand))
-  #                       )
-  #   }
-  # })
+  observeEvent(
+    input$allbrandInput,
+    updateSelectInput(session,
+                      inputId = "brandInput",
+                      'Select Brand',
+                      choices = sort(unique(df$brand)),
+                      selected = c(unique(df$brand)))
+  )
+
+  # when choose "Include all categories", update SelectInput to include all categories
+  observeEvent(
+    input$allcategoryInput,
+    updateSelectInput(session,
+                      inputId = "categoryInput",
+                      'Select Product Category',
+                      choices = sort(unique(df$category)),
+                      selected = c(unique(df$category))
+    )
+  )
+  
+  # when choose "Include all color options", update SelectInput to include all colors
+  # observeEvent(
+  #   input$allcolorInput,
+  #   updateSelectInput(session,
+  #                     inputId = "colorInput",
+  #                     'Select Product Color',
+  #                     choices = sort(unique(df$color)),
+  #                     selected = c(unique(df$color))
+  #   )
+  # )
+  
   
   # create graph for total options
   tot <- reactive({
@@ -168,11 +152,11 @@ server <- function(input, output){
   
   totprodplot <- reactive(
     {p1 <- ggplot(tot(),
-                 aes(brand, fill = category)) +
-          geom_bar() + 
+                 aes(reorder(brand, tot_product), tot_product, fill = category)) +
+          geom_bar(stat = "identity") + 
           labs(title = "Total Options Per Brand",
+               x = "",
                y = "options") +
-          # scale_color_brewer(name = "", palette = "Set1") + 
           coord_flip() + 
           theme_bw()
     return(p1)
@@ -186,11 +170,11 @@ server <- function(input, output){
   # create graph for price range
   priceplot <- reactive(
     {p2 <- ggplot(filtered(),
-                 aes(brand, price, group = category, color = category)) +
+                 aes(reorder(brand, price), price, color = category)) +
       geom_boxplot() + 
       labs(title = "Price Range",
+           x = "",
            y = "USD") + 
-      # scale_color_brewer(name = "", palette = "Set1") + 
       coord_flip() + 
       theme_bw()
     return(p2)
@@ -204,16 +188,18 @@ server <- function(input, output){
   # create graph for average rating
   ar <- reactive({
     filtered() %>% 
-      group_by(brand) %>%
+      group_by(brand, category) %>%
       summarise(avg_rating = mean(rating))
   })
   
   avgplot <- reactive(
-    {p3 <- ggplot(ar(), aes(brand, avg_rating, fill = brand)) +
-          geom_bar(stat = "identity") +
-          scale_color_brewer(name = "", palette = "Set1") +
-          labs(title = "Average Rating", y = "average rating") + 
+    {p3 <- ggplot(ar(), aes(reorder(brand, avg_rating), avg_rating)) +
+          geom_col(alpha = 0.6, color = "black") +
+          labs(title = "Average Rating", 
+               x = "",
+               y = "average rating") + 
           coord_flip() + 
+          facet_wrap(~category) +
           theme_bw()
      return(p3)
     }
